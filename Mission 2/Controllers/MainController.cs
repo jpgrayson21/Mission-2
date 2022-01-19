dbcf7e185f8ef8da5b45d0eaf897c56dfb549204
James Grayson <jaygray@byu.edu>
2022-01-19 14:02:58 -0700

    everything, minus validation

---
+++ Mission 2/Controllers/MainController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_2.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission 2/Models/GradeCalculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_2.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        public float Assignment { get; set; }
+        public float GroupProject { get; set; }
+        public float Quizzes { get; set; }
+        public float Exams { get; set; }
+        public float Intex { get; set; }
+    }
+}
